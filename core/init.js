046f10abc781cb0abc10cd107d5cf095b9f09f05
Echo <mail@flyce.cn>
2020-03-18 22:53:04 +0800

    doc: 删除无用的注释

--- core/init.js
+++ core/init.js
@@ -10,11 +10,10 @@ class InitManager {
         InitManager.loadConfig();
     }
 
-    // 导入的路由必须以moudle.exports = router的形式导出
     /***
      * 初始化加载路由
      * 路由必须以两种方式导出：
-     * 1. module.exports = router 此形式导出的router必须是Router实例化的
+     * 1. module.exports = router 此形式导出的router必须是实例化的Router
      * 2. module.exports = { configRouter } 配置路由方式必须以此格式导出
      */
     static initLoadRouters() {
